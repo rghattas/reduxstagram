1c1f49790ab685b722919eb14a133c21b9d34796
Renee Ghattas <renee.ghattas@qlik.com>
2017-05-28 15:50:21 -0400

    First commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render } from 'react-dom'
+import css from './styles/style.styl'
+
+import App from './components/App'
+import PhotoGrid from './components/PhotoGrid'
+import Single from './components/Single'
+
+import { Router, Route, IndexRoute } from 'react-router'
+
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path='/view/:postId' component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+render(router, document.getElementById('root'))
